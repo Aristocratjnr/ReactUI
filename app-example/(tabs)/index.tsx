73f6ec9182c0da618db7b328573705eb1024f26d
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-06-26 22:15:18 +0000

    chore: Add ThemeProvider to app-example/(tabs)/index.tsx

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import { ThemeProvider } from './ThemeContext';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,6 +15,7 @@ export default function index() {
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
+      </ThemeProvider>
     </NavigationContainer>
   );
 }
